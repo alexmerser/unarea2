eaf5c7da772aeaaf23f0a497cc9c621745966c4a
Alex <you@example.com>
2015-07-11 19:02:00 +0200

    *_*/

--- devctl/stageVM.py
+++ devctl/stageVM.py
@@ -1,55 +1,76 @@
-# DEPLOY STAGE
-# the user to use for the remote commands
-#
-# env.user = 'uadmin'
-# the servers where the commands are executed
-# env.hosts = ['172.127.0.50']
-# env.password = "uadmin1pass"
-# from fabric.operations import run, sudo, require
-# from fabric.context_managers import cd, env
-# from fabric.contrib.files import exists
-# from fabric.colors import red
-from fabric.api import task, run, sudo, env, require, cd
+from fabric.api import task, run, sudo, env, cd
 from fabric.colors import red
+from fabric.context_managers import settings
 from fabric.contrib.files import exists
+from fabric.operations import error
 
+"""
+Stage VM tasks environment.
+To create task-specific env just
+create simple task with all env* definitions.
+
+Example task:
 
 @task
-def stage():
-    """
-    Environment settings for stage VM management.
-    Usage: fab stage <task_name>
-
-    """
+def stageVM():
     env.name = 'stage'
-    env.type = 'STAGE'
-    env.project_root = '/home/unarea/'
-    env.server_root = env.project_root + 'unarea-server'
-    env.repository = 'git@bitbucket.org:unarea/unarea-server.git'
     env.hosts = ['172.127.0.50']
-    env.user = 'uadmin'
-    env.password = 'uadmin1pass'
-    env.branch = 'develop'
+
+"""
+env.name = 'stage'
+env.type = 'STAGE'
+env.project_root = '/home/unarea/'
+env.server_root = env.project_root + 'unarea-server'
+env.repository = 'git@bitbucket.org:unarea/unarea-server.git'
+env.hosts = ['172.127.0.50']
+env.user = 'uadmin'
+env.password = 'uadmin1pass'
+env.branch = 'develop'
 
 
 @task
 def init():
-    if not exists(env.server_root):
-        run('git clone %(repository)s' % env)
+    if exists(env.server_root):
+        error(red("%s exits! Aborting" % env.server_root))
+
+    run('git clone %(repository)s' % env)
+    setup_env()
     with cd(env.server_root):
         run('git checkout %(branch)s; git pull origin %(branch)s;' % env)
-        run('export UNAREA_ENV_TYPE=%(type)s' % env)
         run('python bootstrap.py')
         run('bin/buildout')
+    configure_parts()
+    with settings(warn_only=True):
         start_supervisor()
+    start_apps()
+    restart_network()
+    restart_nginx()
 
+
+@task
+def setup_env():
+    run('touch ~/.bash_profile')
+    run('echo "export UNAREA_HOME=%(project_root)s" >> ~/.bash_profile' % env)
+    run('echo "export UNAREA_APP_ID=SERVER-V2" >> ~/.bash_profile')
+    run('echo "export UNAREA_ENV_TYPE=%(type)s" >> ~/.bash_profile' % env)
+    run('echo "export UNAREA_APP_HOME=%(server_root)s" >> ~/.bash_profile' % env)
+    run('source ~/.bash_profile')
+
+
+@task
+def configure_parts():
+    with cd(env.server_root):
+        sudo('rm -rf /etc/nginx/sites-enabled/unarea-server.conf')
+        sudo('cp etc/nginx_unarea_server.conf /etc/nginx/sites-available/')
+        if not exists('/etc/nginx/sites-enabled/nginx_unarea_server.conf'):
+            sudo('ln -s /etc/nginx/sites-available/nginx_unarea_server.conf /etc/nginx/sites-enabled/')
+        restart_nginx()
 
 @task
 def deploy():
     """
     Usage: $>fab <env_name> deploy
     """
-    require('name')
     shutdown_apps()
     git_pull()
     build_server()
@@ -73,16 +94,6 @@ def build_server():
 
 
 @task
-def configure_parts():
-    with cd(env.server_root):
-        sudo('rm -rf /etc/nginx/sites-enabled/unarea-server.conf')
-        sudo('cp etc/nginx_unarea_server.conf /etc/nginx/sites-available/')
-        if not exists('/etc/nginx/sites-enabled/nginx_unarea_server.conf'):
-            sudo('ln -s /etc/nginx/sites-available/nginx_unarea_server.conf /etc/nginx/sites-enabled/')
-        restart_nginx()
-
-
-@task
 def restart_nginx():
     """Restart the web server"""
     sudo("/etc/init.d/nginx restart")
@@ -102,16 +113,6 @@ def restart_network():
 def shutdown_apps():
     with cd(env.server_root):
         run('bin/supervisorctl stop all')
-
-
-@task
-def setup_env():
-    run('touch ~/.bash_profile')
-    run('echo "export UNAREA_HOME=%(project_root)s" >> ~/.bash_profile' % env)
-    run('echo "export UNAREA_APP_ID=SERVER-V2" >> ~/.bash_profile')
-    run('echo "export UNAREA_ENV_TYPE=%(type)s" >> ~/.bash_profile' % env)
-    run('echo "export UNAREA_APP_HOME=%(server_root)s" >> ~/.bash_profile' % env)
-    run('source ~/.bash_profile')
 
 
 @task
@@ -142,6 +143,7 @@ def status():
         if 'FATAL' in results:
             print red(results)
 
+@task
 def destroy_server():
     if exists(env.server_root):
         run('rm -rf %(server_root)s' % env)
